36ebc363d157a3dbbd379d2c65aaaf116b0a0d73
Ibrahim KIVANC <ikivanc@microsoft.com>
2015-01-09 02:00:16 +0200

    meateDB
    First upload of winrt universal noise level meter app

---
+++ ikivancdBSample/ikivancdBSample.Shared/DataModel/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace ikivancdBSample
+{
+    public class TodoItem
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+
+        [CreatedAt]
+        public DateTimeOffset? CreatedAt { get; set; }
+
+    }
+}
